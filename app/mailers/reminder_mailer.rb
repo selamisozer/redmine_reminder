8759638252eb3636201e6bdf8c123dbd46075ae8
Selami Sözer <selamisozer@gmail.com>
2026-02-12 23:20:40 +0300

    Updated email template with table format and improved styling

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -3,7 +3,6 @@
 class ReminderMailer < ActionMailer::Base
   helper :issues
   helper :custom_fields
-  layout 'mailer'
 
   def reminder_email
     @issue = params[:issue]
@@ -13,8 +12,14 @@ class ReminderMailer < ActionMailer::Base
     @assignee = params[:assignee]
 
     subject_text = Setting.plugin_reminder[:email_subject] || '[Redmine] Overdue Issue Reminder'
-    subject_text = "#{subject_text} - Escalation" if @reminder_type == :second && @assignee
+    subject_text = "#{subject_text} - Eskalasyon" if @reminder_type == :second && @assignee
 
-    mail(to: @recipient.mail, subject: "#{subject_text}: #{@issue.tracker.name} ##{@issue.id} - #{@issue.subject}")
+    @issue_url = "#{Setting.protocol}://#{Setting.host_name}/issues/#{@issue.id}"
+
+    mail(
+      from: Setting.mail_from,
+      to: @recipient.mail,
+      subject: "#{subject_text}: #{@issue.tracker.name} ##{@issue.id} - #{@issue.subject}"
+    )
   end
 end
